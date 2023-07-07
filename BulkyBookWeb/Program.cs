5d0695a85fbcfa767c2a87b82c71d9b9511d7261
Asli Yilmaz <asliyilmaz613@gmail.com>
2023-07-07 21:04:37 +0300

    veritabanı bağlantısı oluşturuldu-code first yaklaşımı ile

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,7 +1,13 @@
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDBContex>(options=>options.UseNpgsql(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
