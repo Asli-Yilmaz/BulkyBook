8c78feeacf4db25008f284e9dc85ac4b664a3098
Asli Yilmaz <asliyilmaz613@gmail.com>
2023-07-10 13:37:23 +0300

    Bootstrap eklendi

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddDbContext<ApplicationDBContex>(options=>options.UseNpgsql(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
